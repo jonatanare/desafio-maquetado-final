d18010ee4fa7c18354b791188fbfc238f2d1657b
Cintia Ruiz <cintiaruizv@gmail.com>
2022-09-18 17:29:28 -0500

    Advance

---
+++ js/createPost.js
@@ -0,0 +1,68 @@
+/* import * as login from './login.js' 
+
+
+const createPost = document.querySelector('#create-post')
+
+btnCreate.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+    createPost.className += 'd-block'
+    main.className = 'd-none'
+})
+
+ */
+
+
+
+
+
+
+
+
+/* const body = document.querySelector( 'body' );
+
+function postBuilder( postStructureId, headerImg, ownerImg, ownerName, postDescription, postTitle, postHashtags, postContent, postComments, postCommentsTitle, buttonSubscribe, userLogedComment, userLogedImg, userCommentInput, allComments ){
+    const postStructureId = document.createElement( 'article' );
+    const headerImg = document.createElement( 'img' );
+    const ownerInfo = document.createElement( 'div' );    
+    const ownerImg = document.createElement( 'img' );
+    const ownerName = document.createElement( 'p' );
+    const postDescription = document.createElement( 'p' );
+    const postTitle = document.createElement( 'h1' );
+    const postHashtags = document.createElement( 'p' );
+    const postContent = document.createElement( 'p' );
+    const postcComments = document.createElement( 'div' );
+    const postCommentsTitle = document.createElement( 'h2' );    
+    const buttonSubscribe = document.createElement( 'button' );
+    const userLogedComment = document.createElement( 'div' );
+    const userLogedImg = document.createElement( 'img' );
+    const userCommentInput = document.createElement( 'input' );
+    const allComments = document.createElement( 'div' );
+
+    article.id = postStructureId;
+    headerImg.src = headerImg;
+    ownerInfo.class = 'ownerInfo';
+    ownerImg,src = ownerImg;
+    ownerName.class = 'OnerName';
+    ownerName.innerText = postowner;
+    postDescription = postDescription;  
+    h1.innerText = postTitle;
+    postHashtags = postHashtags;
+    postContent. innerText = postComment;
+    h2. innerText = postCommentsTitle;
+    postComments.class = 'postComments';
+    buttonSubscribe.class = 'buttonSubscribe';
+    userLogedComment.class = 'userLoged';
+    userLogedComment = userLogedComment;
+    userLogedImg.scr = userLogedImg;
+    userCommentInput.class = 'userComment';
+    allComments.class = 'commentsArea';
+
+    article.appendchild( headerImg, ownerInfo, ownerImg,
+        ownerName, postDescription, h1, postHashtags, postBody, postContent, h2, buttonSubscribe, userLogedComment, userCommentInput, allComments );
+
+    ownerInfo.appendchild( ownerImg, ownerName, postDescription ); 
+    
+    postComments.appendchild( buttonSubscribe,userLogedComment, userLogedImg, userCommentInput );
+
+} */
