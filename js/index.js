165347e80f6e4543baa91748f741283a12ccf26d
Cintia Ruiz <cintiaruizv@gmail.com>
2022-09-19 12:50:14 -0500

    Advance

--- js/index.js
+++ js/index.js
@@ -1,31 +1,7 @@
 import { getPosts } from "./conecction.js";
+import * as login from './login.js'
 
 
-
-/*btnLogin.addEventListener('click', (event) => {
-    event.preventDefault(); 
-    event.stopPropagation(); 
-    login.className += 'd-block'
-    main.className = 'd-none'
-})
-
-btnContinue.addEventListener('click', (event) =>{
-    event.preventDefault(); 
-    event.stopPropagation(); 
-    const email = document.querySelector('[type="email"]').value; 
-    const pass = document.querySelector('[type="password"]').value;
-  if(email === 'movebike@gmail.com' && pass ==='movebike@gmail.com'){
-    login.className = 'd-none'
-    main.className += 'd-block'
-    btnLogin.className = 'd-none'
-    btnCreate.className = 'd-none'
-    btnPost.className += 'd-block'
-    btnNotif.className += 'd-block'
-    btnAvatar.className += 'd-block'
-}else{
-    window.alert('Unable to login. Try again')   
-} 
-})*/
 
 
 
